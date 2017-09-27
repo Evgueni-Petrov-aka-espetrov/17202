a3537eca0125e5bf6c2bde9b5940216aae0ca9cb
v.sazonov <v.sazonov@g.nsu.ru>
2017-09-27 10:22:50 +0700

    third test commit

--- Sazonov/0/01.c
+++ Sazonov/0/01.c
@@ -13,12 +13,13 @@ int input_check(FILE *input_file, char *input_array){
       if(dotnum != -1)
         return 0;
       dotnum = enumerator;
+      continue;
     }
     if(tmp == '\n'){
       endnum = enumerator;
       break;
     }
-    *(input_array+enumerator) = tmp;
+    *(input_array + enumerator) = tmp;
     ++enumerator;
   }
   if(dotnum == endnum - 1 || dotnum == 0 || endnum == 0)
@@ -26,25 +27,16 @@ int input_check(FILE *input_file, char *input_array){
   return 1;
 }
 
-double in_string(char *input_array, int b1, int power){
-  char res;
-  fscanf(input_file, "%c", &res);
-  if(res == '.'){
-    fscanf(input_file, "%c", &res);
-  }
-  if(res == '\n'){
-    return 0;
-  }
+int in_string(char input, int b1, int power){
   int result = 0;
-  res -= '0';
-  if(res > 9){
+  input -= '0';
+  if(input > 9){
     result = 10;
-    res += '0' - 'A';
-
-    if(res > 9)
-      res += 'A' - 'a';
+    input += '0' - 'A';
+    if(input > 9)
+      input += 'A' - 'a';
   }
-  result += res;
+  result += input;
   if(result >= b1 || result < 0)
     return -1;
   return result * pow(b1, power);
@@ -61,15 +53,15 @@ int main(){
     fprintf(output_file, "bad input");
     return 0;
   }
-  char input_array
-  if(input_check(input_file) == 0){
+  char input_array[15];
+  if(input_check(input_file, input_array) == 0){
     fprintf(output_file, "bad input");
     return 0;
   }
   double tmp;
   unsigned long long decimal_c = 0; // хранение целой части ввода, переведенного в десятичный вид
   for(int i = dotnum - 1; i > -1; i--){
-    tmp = in_string(input_file, b1, i);
+    tmp = in_string(input_array[i], b1, i);
     if(tmp == -1){
       fprintf(output_file, "bad input");
       return 0;
@@ -79,7 +71,7 @@ int main(){
   double decimal_d = 0; // хранение дробной части ввода, переведенного в десятичный вид
 
   for(int i = -1; i > (dotnum - endnum); i--){
-    tmp = in_string(input_file, b1, i);
+    tmp = in_string(input_array[i], b1, i);
     printf("inside\n");
     if(tmp == -1){
       fprintf(output_file, "bad input");
