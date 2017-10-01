ef523941b758d63b1187f87db246dbfccd1ffc5d
v.sazonov <v.sazonov@g.nsu.ru>
2017-10-01 20:40:09 +0700

    text

--- Sazonov/0/01.c
+++ Sazonov/0/01.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <Math.h>
 
 int endnum = -1, dotnum = -1;
 
@@ -10,20 +10,16 @@ int input_check(FILE *input_file, char *input_array){
     char tmp;
     fscanf(input_file, "%c", &tmp);
     if(tmp == '.'){
-      if(dotnum != -1)
-        return 0;
+      if(dotnum != -1) return 0;
       dotnum = enumerator;
-      continue;
-    }
-    if(tmp == '\n'){
+    } else if(tmp == '\n'){
       endnum = enumerator;
       break;
     }
     *(input_array + enumerator) = tmp;
     ++enumerator;
   }
-  if(dotnum == endnum - 1 || dotnum == 0 || endnum == 0)
-    return 0;
+  if(dotnum == endnum - 1 || dotnum == 0 || endnum == 0) return 0;
   return 1;
 }
 
@@ -37,8 +33,9 @@ int in_string(char input, int b1, int power){
       input += 'A' - 'a';
   }
   result += input;
-  if(result >= b1 || result < 0)
+  if(result >= b1 || result < 0){
     return -1;
+  }
   return result * pow(b1, power);
 }
 
@@ -53,14 +50,14 @@ int main(){
     fprintf(output_file, "bad input");
     return 0;
   }
-  char input_array[15];
-  if(input_check(input_file, input_array) == 0){
+  char input_array[13];
+  if(input_check(input_file, &input_array[0]) == 0){
     fprintf(output_file, "bad input");
     return 0;
   }
   double tmp;
   unsigned long long decimal_c = 0; // хранение целой части ввода, переведенного в десятичный вид
-  for(int i = dotnum - 1; i > -1; i--){
+  for(int i = dotnum - 1; i > -1; --i){
     tmp = in_string(input_array[i], b1, i);
     if(tmp == -1){
       fprintf(output_file, "bad input");
@@ -70,14 +67,14 @@ int main(){
   }
   double decimal_d = 0; // хранение дробной части ввода, переведенного в десятичный вид
 
-  for(int i = -1; i > (dotnum - endnum); i--){
-    tmp = in_string(input_array[i], b1, i);
-    printf("inside\n");
+  for(int i = -1; i > (dotnum - endnum); --i){
+    tmp = in_string(input_array[dotnum - i], b1, i);
     if(tmp == -1){
       fprintf(output_file, "bad input");
       return 0;
     }
     decimal_d += tmp;
+    printf("%f", decimal_d);
   }
   while(decimal_d >= 1){
     --decimal_d;
@@ -87,12 +84,11 @@ int main(){
   while(power * b2 < decimal_c)
     power *= b2;
   char out[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-  do{
+  do {
     fprintf(output_file, "%c", out[decimal_c / power]);
     decimal_c %= power;
     power /= b2;
-  }while(decimal_c > 0);
-  printf("%f\n", decimal_d);
+  } while(decimal_c > 0);
   if(decimal_d == (double) 0){
     fprintf(output_file, ".0");
   } else {
