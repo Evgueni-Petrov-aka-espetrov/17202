92e9774967454e9e9a1aa61868ff591d173a4821
alexKireevNSU <32298437+alexKireevNSU@users.noreply.github.com>
2017-10-08 13:13:27 +0700

    Проблема  с 16 тестом.
    Вроде бы выводит правильно. Для файла 0123 31230123 выход 21 5 6 7 8. Но тесте говорит, что это неверно

--- Киреев/lab11.cpp
+++ Киреев/lab11.cpp
@@ -1,14 +1,24 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <string>
 #include <conio.h>
+#include <iostream>
 using namespace std;
+
+
 FILE *openInputStream() {//особая магия
 	FILE *stream;
 	return stream = fopen("in.txt", "r");
 }
 
-char rusLang(char tmpCh) {// колдовство
-	return tmpCh + 96 + 224;
+string readTemp(string str, FILE *stream) {
+	while (true)
+	{
+		char tmpCh;
+		fscanf(stream, "%c", &tmpCh);
+		if (tmpCh == '\n') return str;
+		str += tmpCh;
+	}
+	return str;
 }
 
 string readStr(string str, FILE *stream) {
@@ -16,30 +26,33 @@ string readStr(string str, FILE *stream) {
 	{
 		char tmpCh;
 		fscanf(stream, "%c", &tmpCh);
-		//if (tmpCh < 0) { tmpCh = rusLang(tmpCh);}если русские символы, то колдуем
-		if (tmpCh == '\n') return str;
 		if (feof(stream)) {
 			fclose(stream);
 			return str;
 		}
-		str += tmpCh;
+		str+=tmpCh;
 	}
 	return str;
 }
 void printString(string str) {
-	for (char i = 0; i < str.length(); ++i) {
+	for (int i = 0; i < str.length(); ++i) {
 		printf_s("%c", str[i]);
 	}
 }
 int hashStr(string str, char first, char last) {
 	int result= 0;
-	for (char i = first; i <= last; ++i) {
-		result += (str[i] % 3)*(pow(3, i-first));
+	for (int i = first; i <= last; ++i) {
+		if (str[i] >= 0) {
+			result += (str[i] % 3)*(pow(3, i - first));
+		}
+		if (str[i] < 0) {
+			result += ((str[i]+127) % 3)*(pow(3, i - first));
+		}
 	}
 	return result;
 }
-void writeResult(char firstChar, char lastChar) {
-	char tmp = firstChar;
+void writeResult(unsigned int firstChar,unsigned int lastChar) {
+	unsigned int tmp = firstChar;
 	printf_s("%i ", tmp + 1);
 	while (tmp != lastChar)
 	{
@@ -48,14 +61,13 @@ void writeResult(char firstChar, char lastChar) {
 	}
 }
 void findTemplate(string temp, string str) {
-	int tempHash = hashStr(temp, 0, temp.length() - 1);
+	unsigned int tempHash = hashStr(temp, 0, temp.length() - 1);
 	printf_s("%i \n", tempHash);
 
-	char firstChar = 0, lastChar = temp.length() - 1;
-	int strHash = hashStr(str, firstChar, lastChar);
+	unsigned int firstChar = 0, lastChar = temp.length() - 1,strHash = hashStr(str, firstChar, lastChar);
 
 	while (lastChar != str.length()) {
-		if (tempHash == strHash) {
+		if (tempHash == strHash && temp[0]==str[firstChar]) {
 			writeResult(firstChar, lastChar);
 		}
 		++firstChar;
@@ -67,8 +79,8 @@ void main() {
 	string temp, str;
 	FILE *stream = openInputStream();
 
-	temp = readStr(temp, stream);
+	temp = readTemp(temp, stream);
 	str = readStr(str, stream);
+
 	findTemplate(temp, str);
-	system("pause");
 }
