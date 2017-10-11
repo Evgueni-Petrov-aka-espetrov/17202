0be9c4ea3f2693462f3922d30a355c980fd0be7c
v.sazonov <v.sazonov@g.nsu.ru>
2017-10-11 19:20:53 +0700

    Возможно, стало быстрее
    (но это не точно)

--- Sazonov/3-1/31.c
+++ Sazonov/3-1/31.c
@@ -1,64 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void inputArray(FILE *input_file, int n, int *array){
+void outputArray(FILE *output_file, int *array, int n) {
     int *arrayend = array + n;
-    for (int *i = array; i < arrayend; ++i) fscanf(input_file, "%d ", i);
+    for (; array < arrayend; ++array) fprintf(output_file, "%d ", *array);
+    fclose(output_file);
+    free(array);
 }
 
-void outputArray(FILE *output_file, int n, int *array){
-    int *arrayend = array + n;
-    for (int *i = array; i < arrayend; ++i) fprintf(output_file, "%d ", *i);
+void swapElements(int *first, int *second) {
+    int buffer = *first;
+    *first = *second;
+    *second = buffer;
 }
 
-void swapElements(int *a, int *b){
-    int t = *a;
-    *a = *b;
-    *b = t;
-}
-
-int partition(int *array, int low_index, int high_index){
+int partition(int *array, int low_index, int high_index) {
     int pivot = array[high_index]; // pivot >= array[k], low_index <= k <= high_index
     int i = low_index - 1; // array[low_index - 1] <= array[k], low_index <= k <= high_index
 
     for (int j = low_index; j < high_index; ++j)
-        if(array[j] <= pivot) swapElements(&array[++i], &array[j]);
+        if (array[j] <= pivot) swapElements(&array[++i], &array[j]);
 
     swapElements(&array[++i], &array[high_index]);
     return i;
 }
 
-void quickSort(int *array, int low_index, int high_index){
-    if(low_index < high_index){
+void quickSort(int *array, int low_index, int high_index) {
+    if (low_index < high_index) {
         int part = partition(array, low_index, high_index);
         quickSort(array, low_index, part - 1);
         quickSort(array, part + 1, high_index);
     }
 }
 
-int main(){
+int *inputArray(int *n) {
     FILE *input_file = fopen("in.txt", "r");
+    fscanf(input_file, "%d", n);
+    int *array = (int *) malloc(*n * sizeof(int));
+    int *arrayend = array + *n;
+    for (; array < arrayend; ++array) fscanf(input_file, "%d ", array);
+    fclose(input_file);
+    quickSort(array, 0, *n - 1);
+    return array;
+}
 
-    if(input_file == NULL){
-        printf("INPUT ERROR");
-        return 0;
-    }
-
-    int n = 0;
-    fscanf(input_file, "%d", &n);
-
-    int* array = malloc(n * sizeof(int));
-
-    if(array == NULL){
-        printf("MALLOC ERROR");
-        return 0;
-    }
-
-    inputArray(input_file, n, array);
-
-    quickSort(array, 0, n - 1);
-    outputArray(fopen("out.txt", "w"), n, array);
-    free(array);
-
+int main() {
+    int n;
+    outputArray(fopen("out.txt", "w"), inputArray(&n), n);
+    
     return 0;
 }
