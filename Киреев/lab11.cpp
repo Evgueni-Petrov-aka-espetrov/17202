86f5eec1851ed61553d1b4c39f872d843ccab060
alexKireevNSU <32298437+alexKireevNSU@users.noreply.github.com>
2017-10-18 09:07:39 +0700

    Обновление. Не проходит последний тест

--- Киреев/lab11.cpp
+++ Киреев/lab11.cpp
@@ -2,85 +2,115 @@
 #include <string>
 #include <conio.h>
 #include <iostream>
+#include <vector>
 using namespace std;
-
 
 FILE *openInputStream() {//особая магия
 	FILE *stream;
 	return stream = fopen("in.txt", "r");
 }
-
-string readTemp(string str, FILE *stream) {
-	while (true)
-	{
+string readTempFromStream(FILE *stream) {
+	string str;
+	while (true) {
 		char tmpCh;
-		fscanf(stream, "%c", &tmpCh);
-		if (tmpCh == '\n') return str;
+		tmpCh = getc(stream);
+		if (tmpCh == '\n') { break; }
 		str += tmpCh;
 	}
 	return str;
 }
-
-string readStr(string str, FILE *stream) {
-	while (true)
-	{
+string readStrFromStream(FILE *stream, const int length) {
+	string str;
+	int i = 0;
+	while (i < length) {
 		char tmpCh;
-		fscanf(stream, "%c", &tmpCh);
-		if (feof(stream)) {
-			fclose(stream);
-			return str;
-		}
-		str+=tmpCh;
+		tmpCh = getc(stream);
+		str += tmpCh;
+		++i;
 	}
 	return str;
 }
-void printString(string str) {
-	for (int i = 0; i < str.length(); ++i) {
-		printf_s("%c", str[i]);
-	}
+char readNextCh(FILE *stream) {
+	char tmpCh;
+	tmpCh = getc(stream);
+	return tmpCh;
 }
-int hashStr(string str, char first, char last) {
-	int result= 0;
-	for (int i = first; i <= last; ++i) {
+string changeStr(const string &str, const char nextChar) {
+	return str.substr(1) + nextChar;
+}
+
+int hashStr(const string &str, const vector<int> &pows) {
+	int result = 0;
+	for (int i = 0; i < str.length(); ++i) {
 		if (str[i] >= 0) {
-			result += (str[i] % 3)*(pow(3, i - first));
+			result += (str[i] % 3)*pows[i];
 		}
 		if (str[i] < 0) {
-			result += ((str[i]+127) % 3)*(pow(3, i - first));
+			result += ((str[i] + 127) % 3)*pows[i];
 		}
 	}
 	return result;
 }
-void writeResult(unsigned int firstChar,unsigned int lastChar) {
-	unsigned int tmp = firstChar;
-	printf_s("%i ", tmp + 1);
-	while (tmp != lastChar)
-	{
-		printf_s("%i ", tmp + 2);
-		++tmp;
+
+int hashNextStr(const string &str, const int prevHash, const char nextChar, const vector<int> &pows) {
+	if (str[0] >= 0 && nextChar >=0) {
+		return (prevHash - (str[0] % 3)*pows[0]) / 3 + (nextChar % 3)*pows[str.length() - 1];
+	}
+	if (str[0] < 0 && nextChar >= 0) {
+		return (prevHash - ((str[0]+127) % 3)*pows[0]) / 3 + (nextChar % 3)*pows[str.length() - 1];
+	}
+	if (str[0] >= 0 && nextChar < 0) {
+		return (prevHash - (str[0] % 3)*pows[0]) / 3 + ((nextChar + 127) % 3)*pows[str.length() - 1];
+	}
+	if (str[0] < 0 && nextChar < 0) {
+		return (prevHash - ((str[0] + 127) % 3)*pows[0]) / 3 + ((nextChar+127) % 3)*pows[str.length() - 1];
 	}
 }
-void findTemplate(string temp, string str) {
-	unsigned int tempHash = hashStr(temp, 0, temp.length() - 1);
-	printf_s("%i \n", tempHash);
 
-	unsigned int firstChar = 0, lastChar = temp.length() - 1,strHash = hashStr(str, firstChar, lastChar);
-
-	while (lastChar != str.length()) {
-		if (tempHash == strHash && temp[0]==str[firstChar]) {
-			writeResult(firstChar, lastChar);
+void writeResult(const unsigned int firstChar, const unsigned int lastChar, const string &str, const string &temp) {
+	unsigned int tmp = firstChar;
+	printf_s("%i ", tmp + 1);
+	int i = 0;
+	while (tmp != lastChar)
+	{
+		if (temp[i] != str[i]) {
+			break;
 		}
+		printf_s("%i ", tmp + 2);
+		++tmp;
+		++i;
+	}
+}
+
+void RabinKarp(FILE* stream, string str, const string &temp) {
+
+
+	int firstChar = 0, lastChar = temp.length() - 1;
+
+	vector<int> pows(temp.length());
+	for (int i = 0; i < temp.length(); ++i) {
+		pows[i] = pow(3, i);
+	}
+	int tempHash = hashStr(temp, pows);
+	printf_s("%i ", tempHash);
+	int strHash = hashStr(str, pows);
+	while (!feof(stream))
+	{
+		if (tempHash == strHash) {
+			writeResult(firstChar, lastChar, str, temp);
+		}
+		char nextChar = readNextCh(stream);
+		strHash = hashNextStr(str, strHash, nextChar, pows);
+		str = changeStr(str, nextChar);
+		//cout << strHash << ";";
+		/*strHash = hashStr(str, pows);*/
 		++firstChar;
 		++lastChar;
-		strHash = hashStr(str, firstChar, lastChar);
 	}
 }
 void main() {
-	string temp, str;
 	FILE *stream = openInputStream();
-
-	temp = readTemp(temp, stream);
-	str = readStr(str, stream);
-
-	findTemplate(temp, str);
+	string temp = readTempFromStream(stream), str = readStrFromStream(stream, temp.length());
+	RabinKarp(stream, str, temp);
+	//system("pause");
 }
