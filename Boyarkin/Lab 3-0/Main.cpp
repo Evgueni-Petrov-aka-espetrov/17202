52303bc3fe2b20851b7d00c2075be3a0b4530c68
Evgueni Petrov <evgueni.s.petrov@gmail.com>
2017-11-10 18:39:44 +0700

    Update Main.cpp
    fix style

--- Boyarkin/Lab 3-0/Main.cpp
+++ Boyarkin/Lab 3-0/Main.cpp
@@ -1,7 +1,6 @@
-#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
 #pragma warning (disable : 4996)
-
-using namespace std;
 
 void heap_sort(int *arr, int size);
 void heapify(int *arr, int size, int i);
@@ -13,35 +12,38 @@ int main() {
 	FILE *in = fopen("in.txt", "r");
 	FILE *out = fopen("out.txt", "w");
 	
-	int *arr;
-	int n;
-
 	if (in == NULL) {
 		fprintf(out, "Couldn't open file\n");
 		goto END;
 	}
 
+	int n;
 	fscanf(in, "%d", &n);
 	if (n < 0) {
 		fprintf(out, "n is negative!\n");
 		goto END;
 	}
 
-	arr = new int[n];
-	for (int i = 0; i < n; ++i) { fscanf(in, "%d", arr + i); }
+	int* arr = (int*)malloc(sizeof(arr[0]) * n);
+	if (arr == NULL) {
+		fprintf(out, "out of memory!\n");
+		goto END;
+	}
+	for (int i = 0; i < n; ++i) {
+		fscanf(in, "%d", &arr[i]);
+	}
 
 	heap_sort(arr, n);
 
 	setvbuf(out, NULL, _IOFBF, BUF_SIZE);
 	for (int i = 0; i < n; ++i) {
 		fprintf(out, "%d ", arr[i]);
-
 		if (i % 1024 == 0) {
 			fflush(out);
 		}
 	}
 
-	delete[] arr;
+	free(arr);
 
 END:
 	fclose(in);
@@ -50,33 +52,32 @@ END:
 }
 
 void heap_sort(int *arr, int size) {
-
-	int tempBound;
-
 	for (int i = size - 1; i >= 0; --i) {
 		heapify(arr, size, i);
 	}
 
 	while (size > 0) {
 		heapify(arr, size, 0);
-
 		swap(&arr[--size], &arr[0]);
 	}
 }
 
 void heapify(int *arr, int size, int i) {
-	int left;
-	int right;
-	int largest = i;
-	
 	while (true) {
-		left = (i << 1) + 1;
-		right = left + 1;
+		int left = (i << 1) + 1;
+		int right = left + 1;
+		int largest = i;
 
-		if ((left < size) && (arr[left] > arr[largest])) { largest = left; }
-		if ((right < size) && (arr[right] > arr[largest])) { largest = right; }
+		if (left < size && arr[left] > arr[largest]) {
+			largest = left;
+		}
+		if (right < size && arr[right] > arr[largest]) {
+			largest = right;
+		}
 
-		if (largest == i) { break; }
+		if (largest == i) {
+			break;
+		}
 
 		swap(&arr[i], &arr[largest]);
 		i = largest;
@@ -84,7 +85,9 @@ void heapify(int *arr, int size, int i) {
 }
 
 void swap(int *lhs, int *rhs) {
-	if (lhs == rhs) { return; }
+	if (lhs == rhs) {
+		return;
+	}
 
 	*lhs ^= *rhs;
 	*rhs ^= *lhs;
