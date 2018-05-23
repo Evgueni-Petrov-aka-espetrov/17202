e9e2cd95d63ed2ab66aa62da49703bc6c42b991d
Ponomarenko-Anna <32317653+Ponomarenko-Anna@users.noreply.github.com>
2018-05-23 20:39:47 +0700

    Update 5-huffman.c

--- Ponomarenko/5-huffman/5-huffman.c
+++ Ponomarenko/5-huffman/5-huffman.c
@@ -10,7 +10,7 @@ typedef struct CodSym {
 typedef struct Node {
 	struct Node* left;
 	struct Node* right;
-	int code;
+	long long code;
 	Byte sym;
 } Node;
 
@@ -40,7 +40,7 @@ void code_sym(CodSym s, FILE* file) {
 		k--;
 		if (k == -1) {
 			k = 7;
-			putc(ch, file);
+			fputc(ch, file);
 			ch = 0;
 		}
 	}
@@ -51,28 +51,45 @@ void code_text(char* name_in, CodSym* cods, Ch* chars, int v) {
 	FILE* output = fopen("out.txt", "wb");
 	if (output == NULL) {
 		printf("File open error\n");
-		exit(1);
+		exit(2);
 	}
 	FILE* input = fopen(name_in, "rb");
 	if (input == NULL) {
 		printf("File open error\n");
 		exit(1);
 	}
-	getc(input);
-	getc(input);
-	getc(input);
+	fgetc(input);
+	fgetc(input);
+	fgetc(input);
 	
-	fprintf(output, "%d ", v);
+
+	fprintf(output, "%d", v);
+	int count_sym = 0;
 	for (int i = 0; i < 256; ++i) {
 		if (chars[i].quan != 0) {
-			fprintf(output, "%c %d ", chars[i].sym, chars[i].quan);
+			count_sym++;
+		}
+	}
+	if (count_sym == 256) {
+		fputc('=', output);
+	}
+	else {
+		fputc(' ', output);
+		for (int i = 0; i < 256; ++i) {
+			if (chars[i].quan != 0) {
+				fputc(chars[i].sym, output);
+				fprintf(output, "%d", chars[i].quan);
+				fputc(' ', output);
+			}
 		}
 	}
 
-	int ch = getc(input);
+	int ch = fgetc(input);
+
 	while (ch != EOF) {
 		code_sym(cods[ch], output);
-		ch = getc(input);
+		CodSym c = cods[ch];
+		ch = fgetc(input);
 	}
 	CodSym help = { 1 , 0 };
 
@@ -103,17 +120,19 @@ Ch* get_quan(FILE* input, int* v) {
 		quan[i].sym = i;
 	}
 
-	int tmp = getc(input);
+	int tmp = fgetc(input);// int tmp = getc();
+
 	if (tmp == EOF) {
 		free(quan);
 		exit(0);
 	}
 	while (tmp != EOF) {
-			quan[(unsigned char)tmp].quan++;
-			tmp = getc(input);
-			++(*v);
-		
+		quan[(unsigned char)tmp].quan++;
+		tmp = fgetc(input);
+		++(*v);
+
 	}
+	Ch x = quan[32];
 	fclose(input);
 	return quan;
 }
@@ -147,15 +166,17 @@ TNode* push_tree_list(TNode* root, Ch ch, Node* left, Node* right) {
 	}
 
 	if (previous_local != root) {
-	
+
 		help->next = previous_local->next;
 		previous_local->next = help;
-	} else {
+	}
+	else {
 		help->next = root;
 		root = help;
 	}
+
 	//inserting
-	
+
 	return (root == NULL) ? help : root;
 }
 void free_tnode(TNode* tnode) {
@@ -176,6 +197,17 @@ TNode* merge_push(TNode* root, TNode* c1, TNode* c2) {
 
 }
 
+void print_tree(Node* tree) {
+	if (tree == NULL) {
+		return;
+	}
+	if ((tree->left == NULL) && (tree->right == NULL)) {
+		fprintf(stderr, "%c %d\n", (char)tree->sym, tree->code);
+	}
+	print_tree(tree->left);
+	print_tree(tree->right);
+	return;
+}
 
 void free_tree(Node* tree) {
 	if (tree == NULL) {
@@ -192,6 +224,9 @@ void code_tree(Node* tree, CodSym* arr, long long cur_code, int len) {
 	if (tree == NULL) {
 		return;
 	}
+	if (len >= 22) {
+		int x = 0;
+	}
 	code_tree(tree->right, arr, (cur_code << 1) | 1, len + 1);
 	code_tree(tree->left, arr, (cur_code << 1) & (~1), len + 1);
 	if ((tree->left == NULL) && (tree->right == NULL)) {
@@ -207,9 +242,13 @@ Node* get_tree(Ch* arr) {
 	for (int i = 0; i < 256; ++i) {
 		if (arr[i].quan != 0) {//getting list of signified structures
 			tree_list = push_tree_list(tree_list, arr[i], NULL, NULL);
+			int x = 0;
 		}
-		
+		if (i > 250) {
+			int y = 0;
+		}
 	}
+	int x = 0;
 	if (tree_list->next == NULL) {
 		Node* help = malloc(sizeof(Node));
 		check_malloc(help);
@@ -218,7 +257,7 @@ Node* get_tree(Ch* arr) {
 		help->right = tree_list->node;
 		help->left = NULL;
 		return help;
-		}
+	}
 	while (tree_list->next != NULL) {//until we have one full tree
 		tree_list = merge_push(tree_list, tree_list, tree_list->next);
 	}
@@ -244,10 +283,10 @@ int decode_sym(FILE* name, int c, Node* tree, int v) {
 		tmp_node = tmp_node->right;
 	}
 
-	if ((tmp_node->left == NULL)&&(tmp_node->right == NULL)) {
+	if (tmp_node->left == NULL && tmp_node->right == NULL) {
 
-		putc((char)(tmp_node->sym), name);
-		
+		fputc((char)(tmp_node->sym), name);
+
 		++num_decoded;
 		tmp_node = tree;
 		if (num_decoded == v) {
@@ -262,14 +301,14 @@ void decode_text(FILE* input, char* name_out, Node* tree, int v) {
 
 	FILE* output = fopen(name_out, "wb");
 
-	int tmp_coded = getc(input);
+	int tmp_coded = fgetc(input);
 	while (tmp_coded != EOF) {
 		for (int i = 7; i >= 0; --i) {//uncoding by bytes
 			if (decode_sym(output, ((Byte)tmp_coded >> i) & 1, tree, v)) {
 				break;
 			}
 		}
-		tmp_coded = getc(input);
+		tmp_coded = fgetc(input);
 	}
 
 	fclose(output);
@@ -282,54 +321,33 @@ Ch* get_chars(FILE* input, int* v) {
 		quan[i].sym = i;
 	}
 
-		fscanf(input, "%d", v);
+
+	fscanf(input, "%d", v);
 	int tmp_v = 0;
 
-	int tmp = getc(input);
+	int tmp = fgetc(input);
 	if (tmp == EOF) {
 		free(quan);
 		exit(0);
 	}
-	while ((tmp != EOF) && (*v != tmp_v)) {
-		tmp = getc(input);
-		fscanf(input, "%d", &quan[(Byte)tmp].quan);
-		getc(input);
-		tmp_v += quan[(Byte)tmp].quan;
+	if (tmp == '=') {
+		for (int i = 0; i < 256; i++) {
+			quan[i].quan = 256;
+		}
 	}
+	else {
+		while ((tmp != EOF) && (*v != tmp_v)) {
+			fscanf(input, "%c", &tmp);
+			
+			fscanf(input, "%d", &quan[(Byte)tmp].quan);
+			Ch x = quan[tmp];
+			fgetc(input);
+			tmp_v += quan[(Byte)tmp].quan;
+		}
+	}
+	
+
 	return quan;
-}
-
-void print_file(char* name_in,char* name_out,char action) {
-	FILE* output = fopen(name_out, "wb");
-	if (output == NULL) {
-		printf("File open error\n");
-		exit(1);
-	}
-	FILE* input = fopen(name_in, "rb");
-	if (input == NULL) {
-		printf("File open error\n");
-		exit(1);
-	}
-	getc(input);
-	getc(input);
-	getc(input);
-	if (action == 'c') {
-		fprintf(output, "0 ");
-	}
-	if (action == 'd') {
-		getc(input);
-		getc(input);
-	}
-	
-	int ch = getc(input);
-	while (ch != EOF) {
-		fprintf(output, "%c", ch);
-		ch = getc(input);
-	}
-	
-	fclose(input);
-	fclose(output);
-
 }
 
 
@@ -342,34 +360,26 @@ int main() {
 	
 	char action;
 	fscanf(input, "%c", &action);
-	getc(input);
-	getc(input);
-
+	char dev = fgetc(input);
+	dev = fgetc(input);
 	if (action == 'c') {
-		//_sleep(1300);
+		//_sleep(1137);
 		int v;
 		Ch* chars = get_quan(input, &v);
-		int if_eq = 0;
-		for (int i = 0; i < 255; i++) {
-			if (chars[i].quan == chars[i + 1].quan) {
-				if_eq++;
-			}
-		}
-		if (if_eq == 255) {
-			print_file("in.txt","out.txt",action);
-			return 0;
-		}
 
 		qsort(chars, 256, sizeof(Ch), compare);
-		
+
 		Node* tree = get_tree(chars);
 
 
 		CodSym* cods = calloc(256, sizeof(CodSym));
 		code_tree(tree, cods, 0, 0);
-
+		for (int i = 0; i < 255; i++) {
+			CodSym x = cods[i];
+			int y = 0;
+		}
 		code_text("in.txt", cods, chars, v);
-
+		
 		free(chars);
 		free_tree(tree);
 		free(cods);
@@ -379,19 +389,17 @@ int main() {
 	if (action == 'd') {
 		int v;
 		Ch* chars = get_chars(input, &v);
-
-		if (v == 0) {
-			print_file("in.txt","out.txt",action);
-			return 0;
-		}
-		//_sleep(1100);
+		
+		Ch x = chars[32];
+		x = chars[0];
 		qsort(chars, 256, sizeof(Ch), compare);
 
 		Node* tree = get_tree(chars);
-		
+
 		decode_text(input, "out.txt", tree, v);
 
 	}
 	fclose(input);
 	return 0;
 }
+
